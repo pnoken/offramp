604fb23ca98fe1344fe816a2d3edaa94e0562b25
pnoken <norteypeter@gmail.com>
2024-08-16 07:23:30 +0000

    chore: frontend project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { Web5 } from '@web5/api';
-import { VerifiableCredential } from '@web5/credentials';
 
 function App() {
   const [loading, setLoading] = useState(false);
+  const [web5, setWeb5] = useState(null);
+  const [myDid, setMyDid] = useState(null);
 
 
-  const createDid = async () => {
-    const { web5, did: userDid } = await Web5.connect();
-    if (!userDid) {
-      setLoading(true)
-    }
-    return userDid
-  }
+  useEffect(() => {
+    const initWeb5 = async () => {
+      setLoading(true);
+      const { web5, did } = await Web5.connect();
+      setWeb5(web5);
+      setMyDid(did);
+
+      if (web5 && did) {
+        setLoading(false);
+        // await configureProtocol(web5, did);
+        // await fetchDings(web5, did);
+      }
+    };
+    initWeb5();
+  }, []);
+
   return (
     <div className="h-lvh p-4">
       <div className="shadow-inner border-gray-200 border-solid border-2 h-full w-full sm:w-1/2 mx-auto rounded-xl">
@@ -35,12 +45,11 @@ function App() {
           <div>
             <button
               type="submit"
-              onClick={createDid}
               className="flex w-full justify-center rounded-xl bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Create New Wallet
             </button>
-            {loading ? <h2>creating did</h2> : <h2>done</h2>}
+            {loading ? <h2>creating did</h2> : <h2>{myDid}</h2>}
           </div>
 
           <div>
