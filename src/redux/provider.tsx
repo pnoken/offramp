9dee893d348d43e451a58fc931be66e9add279c5
pnoken <norteypeter@gmail.com>
2024-09-10 08:07:10 +0000

    fix: add redux provider

---
+++ src/redux/provider.tsx
@@ -0,0 +1,8 @@
+"use client";
+import store from '@/lib/store';
+import React from 'react';
+import { Provider } from 'react-redux';
+
+export function ReduxProvider({ children }: { children: React.ReactNode }) {
+    return <Provider store={store}>{children}</Provider>
+}
