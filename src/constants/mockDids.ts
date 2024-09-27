c3405e210a50c1f2c8384bf93f29c590bef5bb13
pnoken <norteypeter@gmail.com>
2024-09-27 13:02:07 +0000

    fix: cancelling order button

--- src/constants/mockDids.ts
+++ src/constants/mockDids.ts
@@ -4,11 +4,6 @@ export const mockProviderDids = {
         name: 'AquaFinance Capital',
         description: 'Provides exchanges with the Ghanaian Cedis: GHS to USDC, GHS to KES'
     },
-    flowback_financial: {
-        uri: 'did:dht:zkp5gbsqgzn69b3y5dtt5nnpjtdq6sxyukpzo68npsf79bmtb9zy',
-        name: 'Flowback Financial',
-        description: 'Offers international rates with various currencies - USD to GBP, GBP to CAD.'
-    },
 };
 
 
@@ -17,10 +12,5 @@ export const pfiAllowList = [
         uri: 'did:dht:3fkz5ssfxbriwks3iy5nwys3q5kyx64ettp9wfn1yfekfkiguj1y',
         name: 'AquaFinance Capital',
         description: 'Provides exchanges with the Ghanaian Cedis: GHS to USDC, GHS to KES'
-    },
-    {
-        uri: 'did:dht:enwguxo8uzqexq14xupe4o9ymxw3nzeb9uug5ijkj9rhfbf1oy5y',
-        name: 'Vertex Liquid Assets',
-        description: 'Offers currency exchanges between African currencies - MAD to EGP, GHS to NGN.'
     },
 ]
