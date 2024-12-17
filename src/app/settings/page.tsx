654e5ab052e7a013c192be0b0d2bd04a83c402c7
pnoken <norteypeter@gmail.com>
2024-12-17 00:14:54 +0000

    fix(settings): improve settings ui

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -41,66 +41,72 @@ const Settings: React.FC = () => {
   };
 
   return (
-    <div className="p-4 md:p-6 bg-white rounded-lg shadow-md">
-      <h1 className="text-2xl font-bold mb-4">Settings</h1>
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+      <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-4 space-y-6">
+        <div className="flex flex-col justify-between items-center">
+          <h1 className="text-2xl font-bold mb-4">Settings</h1>
 
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-2">Notifications</label>
-        <div className="flex items-center">
-          <input
-            type="checkbox"
-            checked={notificationsEnabled}
-            onChange={handleToggleNotifications}
-            className="mr-2"
-          />
-          <span>
-            Receive notifications on transactions and fiatsend updates
-          </span>
-        </div>
-        {showEmailInput && (
-          <div className="mt-2">
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              placeholder="Enter your email"
-              className="block w-full p-2 border border-gray-300 rounded"
-            />
-            <div className="mt-2 flex justify-between">
-              <button
-                onClick={handleSaveEmail}
-                className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700"
-              >
-                Save
-              </button>
-              <button
-                onClick={handleCancelEmail}
-                className="bg-gray-400 text-white py-1 px-3 rounded hover:bg-gray-500"
-              >
-                Cancel
-              </button>
+          <div className="mb-4">
+            <label className="block text-sm font-medium mb-2">
+              Notifications
+            </label>
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                checked={notificationsEnabled}
+                onChange={handleToggleNotifications}
+                className="mr-2"
+              />
+              <span>
+                Receive notifications on transactions and fiatsend updates
+              </span>
             </div>
+            {showEmailInput && (
+              <div className="mt-2">
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Enter your email"
+                  className="block w-full p-2 border border-gray-300 rounded"
+                />
+                <div className="mt-2 flex justify-between">
+                  <button
+                    onClick={handleSaveEmail}
+                    className="bg-blue-600 text-white py-1 px-3 rounded hover:bg-blue-700"
+                  >
+                    Save
+                  </button>
+                  <button
+                    onClick={handleCancelEmail}
+                    className="bg-gray-400 text-white py-1 px-3 rounded hover:bg-gray-500"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
-        )}
-      </div>
 
-      <div className="mb-4">
-        <label className="block text-sm font-medium mb-2">
-          Set allowance for incoming transactions
-        </label>
-        <input
-          type="number"
-          value={allowance}
-          onChange={handleAllowanceChange}
-          className="block w-full p-2 border border-gray-300 rounded"
-          placeholder="Input amount in dollars"
-        />
-        <button
-          onClick={handleApproveAllowance}
-          className="mt-2 bg-green-600 text-white py-1 px-3 rounded hover:bg-green-700"
-        >
-          Approve
-        </button>
+          <div className="mb-4">
+            <label className="block text-sm font-medium mb-2">
+              Set allowance for incoming transactions
+            </label>
+            <input
+              type="number"
+              value={allowance}
+              onChange={handleAllowanceChange}
+              className="block w-full p-2 border border-gray-300 rounded"
+              placeholder="Input amount in dollars"
+            />
+            <button
+              onClick={handleApproveAllowance}
+              className="mt-2 bg-green-600 text-white py-1 px-3 rounded hover:bg-green-700"
+            >
+              Approve
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
