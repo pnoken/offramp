b4c6b8ac6bd72dedaccb855ec6fe5808fccbed8f
pnoken <norteypeter@gmail.com>
2025-01-01 15:01:21 +0000

    chore(layout): add cookie3 tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import StickyNavbar from "@/components/layout/StickyNavbar";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -39,6 +40,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://cdn.markfi.xyz/scripts/analytics/0.11.21/cookie3.analytics.min.js"
+          integrity="sha384-wtYmYhbRlAqGwxc5Vb9GZVyp/Op3blmJICmXjRiJu2/TlPze5dHsmg2gglbH8viT"
+          crossOrigin="anonymous"
+          async
+          strategy="lazyOnload"
+          site-id="c50f717d-19ed-4031-a736-22ef49a545f0"
+        />
+      </head>
       <body className={inter.className}>
         <QueryClientProvider client={queryClient}>
           <WagmiConfig config={config}>
--- src/constants/contract-address.ts
+++ src/constants/contract-address.ts
@@ -1,2 +1,8 @@
 const MomoNFT = "";
 const FIATSEND_BANKING = "";
+const acceptChainId = [];
+const maxMintAmount = 1;
+const liskSepoliaChainId = 324;
+const mintFee = 0.0009; //ETH
+
+const contracts = {};
