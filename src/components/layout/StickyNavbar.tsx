cd7e2e3cc01fc5541d1aa5ab6c67e222d1debcfb
pnoken <norteypeter@gmail.com>
2025-01-05 18:12:35 +0000

    feat(sticky-navbar): make offramp nav active

--- src/components/layout/StickyNavbar.tsx
+++ src/components/layout/StickyNavbar.tsx
@@ -17,12 +17,11 @@ const StickyNavbar: React.FC = () => {
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-white shadow-lg p-4 flex justify-around items-center border-t border-gray-300 sm:hidden">
       <button
-        disabled
-        onClick={() => handleNavigation("/rewards")}
+        onClick={() => handleNavigation("/")}
         className="flex flex-col items-center opacity-50"
       >
         <HomeIcon className="h-5 w-5 mb-1" aria-hidden="true" />
-        <span className="text-md">Rewards</span>
+        <span className="text-md">Offramp</span>
       </button>
       <button
         onClick={() => handleNavigation("/faucet")}
